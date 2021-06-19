fe29b621a143187bc252cc049815463cdba35051
Kacper Dudek <dudek.kacper2797@gmail.com>
2021-06-19 16:35:49 +0200

    Creating decode function

--- Straddling checkerboard.py
+++ Straddling checkerboard.py
@@ -53,5 +53,24 @@ proba1="Wszechświat ma około czternaście miliardów lat."
 
 print(encode(proba1))
 
+'''Define decode func'''
 
+def decode(proba):
+
+    result=""
+    proba=proba.split(" ")
+    for i,j in enumerate(proba):
+        for k,v in slownik_kodu.items():
+            if j==v:
+                result=result+k
+
+
+
+    return f"Tekst odszyfrowany: {result.capitalize()}"
+
+
+#proba2="813050333881278493279634636693358408547383320932210278537898191078417"
+proba2="81 30 5 0 33 19 38 81 2 7 84 9 32 7 9 6 34 6 36 6 9 33 5 84 0 85 4 7 38 33 2 0 9 32 2 10 2 7 85 37 89 81 9 10 7 84 17"
+
+print(decode(proba2))
 
